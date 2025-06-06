3de5560610a0fc9e7c814029b6221715d2fdbc14
unknown <aslanbekbakberdi@gmail.com>
2025-06-07 04:18:15 +0500

    404

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
